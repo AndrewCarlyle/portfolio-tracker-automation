7d605f91e6c33af74e945b59f2b9c4c3461a1ed2
Andrew Carlyle <andrew.carlyle31@gmail.com>
2023-12-20 09:37:44 -0500

    Add ibkr automation

--- addRspContributions.py
+++ addRspContributions.py
@@ -1,20 +1,21 @@
-import os.path
 import logging
 
-from google.auth.transport.requests import Request
+'''from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from googleapiclient.errors import HttpError'''
+from google.auth.exceptions import RefreshError
+from googleSheetsWriter import GoogleSheetsWriter
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+#SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
 PORTFOLIO_SPREADSHEET_ID = '1M6ZNA_O1-T90ASBvttFo_hNroYqYCJKD_5SAjmBT1wg'
-SAMPLE_RANGE_NAME = 'RSP Contributions!A2:C2'
+APPEND_RANGE = 'RSP Contributions!A2:C2'
 
 # Set up logging
 LOG_DIRECTORY = '/var/log/financeJobs'
@@ -22,6 +23,8 @@ LOG_FILE_PATH = f'{LOG_DIRECTORY}/rspUpdate.log'
 
 #Path to directory where the .json files are stored
 DIR_PATH = '/Users/andre/Workspace/portfolio-tracker-automation'
+
+CONTRIBUTION_AMOUNT = 163.46
 
 logging.basicConfig(
     level=logging.INFO,
@@ -32,67 +35,6 @@ logging.basicConfig(
     ]
 )
 
-class contributionAutomation:
-    def __init__(self, contributionAmount):
-        self.amount = contributionAmount
-
-        self.creds = None
-        self.authenticate()
-
-        self.service = build('sheets', 'v4', credentials=self.creds)
-
-        self.date = getDate()
-
-    def getLastEntryIndex(self):
-        """Shows basic usage of the Sheets API.
-        Prints values from a sample spreadsheet.
-        """
-        try:
-            # Call the Sheets API
-            sheet = self.service.spreadsheets()
-            result = sheet.values().get(spreadsheetId=PORTFOLIO_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
-            values = result.get('values', [])
-
-            if not values:
-                logging.warning('No data found')
-                return
-
-            logging.info('Result:')
-            for row in values:
-                logging.info('%s' % (row[0]))
-        except HttpError as err:
-            print(err)
-    
-    def writeValueToSheet(self):
-        try:
-            # Call the Sheets API
-            sheet = self.service.spreadsheets()
-            result = sheet.values().append(spreadsheetId=PORTFOLIO_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME, valueInputOption='USER_ENTERED', body=dict(
-                majorDimension="ROWS",
-                values=[
-                    [self.date, self.amount, self.amount]
-                ])).execute()
-            logging.info(f"Update result:{result}")
-
-        except HttpError as err:
-            logging.error(err)
-
-    def authenticate(self):
-        # The file token.json stores the user's access and refresh tokens, and is created automatically 
-        # when the authorization flow completes for the first time.
-        if os.path.exists(f'{DIR_PATH}/token.json'):
-            self.creds = Credentials.from_authorized_user_file(f'{DIR_PATH}/token.json', SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not self.creds or not self.creds.valid:
-            if self.creds and self.creds.expired and self.creds.refresh_token:
-                self.creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(f'{DIR_PATH}/credentials.json', SCOPES)
-                self.creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open(f'{DIR_PATH}/token.json', 'w') as token:
-                token.write(self.creds.to_json())
-
 def getDate():
     # Get the current date, then format
     current_date = datetime.now()
@@ -102,10 +44,13 @@ def getDate():
 
 def main():
     try:
-        contrAutomation = contributionAutomation(contributionAmount=163.46)
-        contrAutomation.writeValueToSheet()
+        writer = GoogleSheetsWriter()
+
+        values = [getDate(), CONTRIBUTION_AMOUNT, CONTRIBUTION_AMOUNT]
+
+        writer.appendValueToSheet(PORTFOLIO_SPREADSHEET_ID, APPEND_RANGE, values)
     except Exception as e:
         logging.error(e)
 
-#if __name__ == '__main__':
-main()
+if __name__ == '__main__':
+    main()
---
+++ googleSheetsWriter.py
@@ -0,0 +1,84 @@
+import os.path
+import subprocess
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.auth.exceptions import RefreshError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+#Path to directory where the .json files are stored
+CREDENTIAL_DIR_PATH = '/Users/andre/Workspace/portfolio-tracker-automation'
+
+class GoogleSheetsWriter:
+    def __init__(self):
+        self.creds = None
+        self.authenticate()
+
+        self.service = build('sheets', 'v4', credentials=self.creds)
+    
+    #spreadsheet string the ID of the spreadsheet eg. '1M6ZNA_O1-T90ASBvttFo_hNroYqYCJKD_5SAjmBT1wg'
+    #cellrange string eg. 'Net Worth!B1'
+    #values list of value to write to the spreadsheet at cellRange
+    def writeValueToSheet(self, spreadsheet, cellRange, values):
+        try:
+            # Call the Sheets API
+            sheet = self.service.spreadsheets()
+            result = sheet.values().update(spreadsheetId=spreadsheet, range=cellRange, valueInputOption='USER_ENTERED', body=dict(
+                majorDimension="ROWS",
+                values=[values])
+            ).execute()
+            
+            print(f"Update result:{result}")
+
+        except HttpError as err:
+            print(err)
+
+    #spreadsheet string the ID of the spreadsheet eg. '1M6ZNA_O1-T90ASBvttFo_hNroYqYCJKD_5SAjmBT1wg'
+    #cellrange string eg. 'Net Worth!B1'
+    #values list of value to write to the spreadsheet at cellRange
+    def appendValueToSheet(self, spreadsheet, cellRange, values):
+        try:
+            # Call the Sheets API
+            sheet = self.service.spreadsheets()
+            result = sheet.values().append(spreadsheetId=spreadsheet, range=cellRange, valueInputOption='USER_ENTERED', body=dict(
+                majorDimension="ROWS",
+                values=[values])
+            ).execute()
+            
+            print(f"Update result:{result}")
+
+        except HttpError as err:
+            print(err)
+
+    def authenticate(self):
+        def browserLogin():
+            flow = InstalledAppFlow.from_client_secrets_file(f'{CREDENTIAL_DIR_PATH}/credentials.json', SCOPES)
+            self.creds = flow.run_local_server(port=0)
+
+        # The file token.json stores the user's access and refresh tokens, and is created automatically 
+        # when the authorization flow completes for the first time.
+        if os.path.exists(f'{CREDENTIAL_DIR_PATH}/token.json'):
+            self.creds = Credentials.from_authorized_user_file(f'{CREDENTIAL_DIR_PATH}/token.json', SCOPES)
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                try:
+                    self.creds.refresh(Request())
+                except RefreshError as e:
+                    #Delete existing expired token and re-attempt authentication
+                    command = 'rm -f token.json'
+                    subprocess.run(command, shell=True)
+
+                    browserLogin()
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(f'{CREDENTIAL_DIR_PATH}/credentials.json', SCOPES)
+                self.creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(f'{CREDENTIAL_DIR_PATH}/token.json', 'w') as token:
+                token.write(self.creds.to_json())
